0778210ae11a414bcc01603b125ab3daa8c08e6e
Pery Lemke <pery.lemke@gmail.com>
2023-08-18 11:39:16 -0300

    Implement tests

--- src/functions/post.js
+++ src/functions/post.js
@@ -5,40 +5,52 @@ const AWS = require('aws-sdk');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.createContact = async(event, context, callback) => {
-    const timestamp = new Date().getTime();
-    const req = JSON.parse(event.body);
-    if (typeof req.email !== 'string') {
-        console.error('Validation failed');
-        callback(null, {
-            statusCode: 400,
-            body: JSON.stringify({
-                message: 'Couldn\'t create the contact.'
-            })
-        });
-        return;
-    }
-
-    const params = {
-        TableName: 'contacts',
-        Item: {
-            id: uuid.v1(),
-            name: req.name,
-            email: req.email,
-            createdAt: timestamp,
-            updatedAt: timestamp,
-        },
-    };
-
-    try {
-        await dynamoDb.put(params).promise();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(params.Item)
+module.exports = {
+    dynamoDb: dynamoDb,
+    createContact: async (event, callback) => {
+        const timestamp = new Date().getTime();
+        if (!event.body) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'Validation failed'
+                })
+            }
         }
-    } catch (error) {
-        return {
-            error: error
+
+        const req = JSON.parse(event.body);
+        if (typeof req.email !== 'string' || typeof req.name !== 'string') {
+            console.error('Validation failed');
+            callback(null, {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'Couldn\'t create the contact.'
+                })
+            });
+            return;
+        }
+
+        const params = {
+            TableName: 'contacts',
+            Item: {
+                id: uuid.v4(),
+                name: req.name,
+                email: req.email,
+                createdAt: timestamp,
+                updatedAt: timestamp,
+            },
+        };
+
+        try {
+            await dynamoDb.put(params).promise();
+            return {
+                statusCode: 200,
+                body: JSON.stringify(params.Item)
+            }
+        } catch (error) {
+            return {
+                error: error
+            }
         }
     }
 };
---
+++ tests/post.test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const expect = chai.expect;
+const createContactHandler = require('../src/functions/post');
+
+describe('createContact', () => {
+  it('must be create a new contact', async () => {
+    const mockPutPromise = (params) => {
+      expect(params.Item.name).to.equal('New Contact');
+      expect(params.Item.email).to.equal('new@email.com');
+      return {
+        promise: () => Promise.resolve({})
+      };
+    };
+
+    const event = {
+      body: JSON.stringify({
+        name: 'New Contact',
+        email: 'new@email.com'
+      })
+    };
+
+    const context = {};
+
+    createContactHandler.dynamoDb.put = mockPutPromise;
+
+    const result = await createContactHandler.createContact(event, context);
+
+    expect(result.statusCode).to.equal(200);
+    expect(result.body).to.be.a('string');
+  });
+
+  it('must be returns a error for a body missing', async () => {
+    const event = {};
+    const context = {};
+
+    const result = await createContactHandler.createContact(event, context);
+
+    expect(result.statusCode).to.equal(400);
+    expect(result.body).to.be.a('string');
+  });
+});
